56e94ef119d705c30e0273d14afe6eb3ee4069ed
chen <1728533217en@gmail.com>
2018-08-26 00:50:40 +0800

    update

--- tests/semantics/show_engine.cpp
+++ tests/semantics/show_engine.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+#include<util/dense_bitset.hpp>
+#include<atomic>
 /**
  * the semantics of sync engine
  * activestep
@@ -10,3 +13,43 @@
  *  iter++
  * }
  **/
+class Engine
+{
+
+private:
+    std::atomic<size_t> shared_vid_counter;
+    parallelgraph::dense_bitset db;
+};
+int main(int argc, char const *argv[])
+{
+      //denbse_bitset 只输出有效位
+  parallelgraph::fixed_dense_bitset<128> fdb;
+  parallelgraph::dense_bitset db;
+  
+  fdb.set_bit(0);
+  fdb.set_bit(32);
+  fdb.set_bit(65);
+  
+  for(auto e: fdb)
+    std::cout<<e<<std::endl;
+  // 0 32 65
+
+  db.resize(32);
+  db.set_bit(1);
+  db.set_bit(3);
+  db.set_bit(7);
+  for(auto e: db)
+    std::cout<<e<<std::endl;
+  // 1 3 7
+
+
+  size_t vid = 70;
+  size_t vid_block = fdb.containing_word(vid);
+  parallelgraph::fixed_dense_bitset<64> local_bitset;
+  local_bitset.initialize_from_mem(&vid_block, 8);
+  for(auto e: local_bitset)
+  std::cout<<64+e;
+
+  std::cout<<std::endl;    
+    return 0;
+}
