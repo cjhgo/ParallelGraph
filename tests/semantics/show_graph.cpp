36accd4bf88cf1e2dac0ee48adc88820ad0df9e3
chen <1728533217en@gmail.com>
2018-08-26 23:41:10 +0800

    update

---
+++ tests/semantics/show_graph.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <string>
+#include <graph/graph.hpp>
+#include <fstream>
+using namespace parallelgraph;
+int main(int argc, char const *argv[])
+{
+	std::string file_path="dd";
+	graph<double, double> graphi;
+	graphi.directed=true;
+	graphi.load(file_path, 1.1);
+	graphi.show_graph();
+	return 0;
+}
