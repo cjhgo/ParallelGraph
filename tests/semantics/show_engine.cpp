d91138bae9c8bda7ffea23a54073b0771ebb2732
chen <1728533217en@gmail.com>
2018-08-26 18:59:01 +0800

    engine demo

--- tests/semantics/show_engine.cpp
+++ tests/semantics/show_engine.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<util/dense_bitset.hpp>
 #include<atomic>
+#include<cstdlib>
+#include<ctime>
+#include<pthread.h>
+#define NUM_THREADS 5
+#define NUM_TASKS 64*10000
 /**
  * the semantics of sync engine
  * activestep
@@ -12,44 +17,79 @@
  *  S S S S S ... S#run_multi(S)
  *  iter++
  * }
+ * 
  **/
+typedef void * (*THREADFUNCPTR)(void *);
 class Engine
 {
 
 private:
     std::atomic<size_t> shared_vid_counter;
-    parallelgraph::dense_bitset db;
+    parallelgraph::dense_bitset active;
+    pthread_t threads[NUM_THREADS];
+    void set_task()
+    {
+      std::srand(std::time(nullptr)); 
+      for(int i = 0; i < NUM_TASKS;i++)
+      {
+        double r = ((double) rand() / (RAND_MAX));
+        if(1 or r > 0.6)  
+        {
+          active.set_bit(i);
+        }
+      }
+      
+    };
+public:
+    Engine()
+    {
+      active.resize(NUM_TASKS);
+      set_task();
+      std::cout<<"the count of bit:"<<active.popcount()<<std::endl;
+    };   
+    void task()
+    {
+      while(1)
+      {
+        size_t vid_block_start = shared_vid_counter.fetch_add(8 * sizeof(size_t));                  
+        if (vid_block_start >= NUM_TASKS) break;
+        size_t vid_block = active.containing_word(vid_block_start);
+        parallelgraph::fixed_dense_bitset<64> local_bitset;
+        local_bitset.initialize_from_mem(&vid_block, 8);
+        for(auto e: local_bitset)
+        std::cout<<vid_block_start+e<<std::endl;     
+      }      
+    };
+    template<typename MemberFun>
+    void execute_fun(MemberFun fn)
+    {
+         
+      shared_vid_counter = 0;
+      int rc;   
+      for(int i = 0; i < NUM_THREADS; i++ ) 
+      {
+          std::cout << "creating thread, " << i << std::endl;
+          rc = pthread_create(&threads[i], NULL, (THREADFUNCPTR)fn, (void *)this);
+          if (rc) 
+          {
+            std::cout << "Error:unable to create thread," << rc << std::endl;
+            exit(-1);
+          }
+      }
+    };
+    void run()
+    {
+      execute_fun(&Engine::task);
+      for(int j = 0; j < NUM_THREADS; j++ )
+      {
+        pthread_join(threads[j], NULL);
+      }
+      std::cout<<"in the end: "<<shared_vid_counter<<"..."<<shared_vid_counter/64<<std::endl;
+    };   
 };
 int main(int argc, char const *argv[])
 {
-      //denbse_bitset 只输出有效位
-  parallelgraph::fixed_dense_bitset<128> fdb;
-  parallelgraph::dense_bitset db;
-  
-  fdb.set_bit(0);
-  fdb.set_bit(32);
-  fdb.set_bit(65);
-  
-  for(auto e: fdb)
-    std::cout<<e<<std::endl;
-  // 0 32 65
-
-  db.resize(32);
-  db.set_bit(1);
-  db.set_bit(3);
-  db.set_bit(7);
-  for(auto e: db)
-    std::cout<<e<<std::endl;
-  // 1 3 7
-
-
-  size_t vid = 70;
-  size_t vid_block = fdb.containing_word(vid);
-  parallelgraph::fixed_dense_bitset<64> local_bitset;
-  local_bitset.initialize_from_mem(&vid_block, 8);
-  for(auto e: local_bitset)
-  std::cout<<64+e;
-
-  std::cout<<std::endl;    
-    return 0;
+  Engine engine;
+  engine.run();
+  return 0;
 }
