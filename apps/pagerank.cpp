6d936055628424c911a220c663b31609ed7871c2
chen <1728533217en@gmail.com>
2018-08-28 23:53:01 +0800

    first app to verify framework

--- apps/pagerank.cpp
+++ apps/pagerank.cpp
@@ -1,11 +1,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
-
-#include <graphlab.hpp>
-// #include <graphlab/macros_def.hpp>
-
-// Global random reset probability
+#include <parallelgraph.hpp>
 double RESET_PROB = 0.15;
 
 double TOLERANCE = 1.0E-2;
@@ -17,10 +13,11 @@ size_t ITERATIONS = 0;
 typedef double vertex_data_type;
 
 // There is no edge data in the pagerank application
-typedef graphlab::empty edge_data_type;
+//typedef parallelgraph::empty edge_data_type;
+typedef double edge_data_type;
 
 // The graph type is determined by the vertex and edge data types
-typedef graphlab::distributed_graph<vertex_data_type, edge_data_type> graph_type;
+typedef parallelgraph::graph<vertex_data_type, edge_data_type> graph_type;
 
 /*
  * A simple function used by graph.transform_vertices(init_vertex);
@@ -28,47 +25,27 @@ typedef graphlab::distributed_graph<vertex_data_type, edge_data_type> graph_type
  */
 void init_vertex(graph_type::vertex_type& vertex) { vertex.data() = 1; }
 
-
-
-/*
- * The factorized page rank update function extends ivertex_program
- * specifying the:
- *
- *   1) graph_type
- *   2) gather_type: double (returned by the gather function). Note
- *      that the gather type is not strictly needed here since it is
- *      assumed to be the same as the vertex_data_type unless
- *      otherwise specified
- *
- * In addition ivertex program also takes a message type which is
- * assumed to be empty. Since we do not need messages no message type
- * is provided.
- *
- * pagerank also extends graphlab::IS_POD_TYPE (is plain old data type)
- * which tells graphlab that the pagerank program can be serialized
- * (converted to a byte stream) by directly reading its in memory
- * representation.  If a vertex program does not exted
- * graphlab::IS_POD_TYPE it must implement load and save functions.
- */
 class pagerank :
-  public graphlab::ivertex_program<graph_type, double> {
+  public parallelgraph::ivertex_program<graph_type, double> {
 
   double last_change;
 public:
 
-  /**
-   * Gather only in edges.
-   */
   edge_dir_type gather_edges(icontext_type& context,
                               const vertex_type& vertex) const {
-    return graphlab::IN_EDGES;
+    return parallelgraph::IN_EDGES;
   } // end of Gather edges
 
 
   /* Gather the weighted rank of the adjacent page   */
   double gather(icontext_type& context, const vertex_type& vertex,
-               edge_type& edge) const {
-    return (edge.source().data() / edge.source().num_out_edges());
+               edge_type& edge) const 
+  {
+    vertex_data_type d = edge.source().data();
+    size_t noe = edge.source().num_out_edges();
+    double value = d /noe;
+    return value;
+    //return (edge.source().data() / edge.source().num_out_edges());
   }
 
   /* Use the total rank of adjacent pages to update this page */
@@ -78,34 +55,28 @@ public:
     const double newval = (1.0 - RESET_PROB) * total + RESET_PROB;
     last_change = (newval - vertex.data());
     vertex.data() = newval;
-    if (ITERATIONS) context.signal(vertex);
   }
 
   /* The scatter edges depend on whether the pagerank has converged */
   edge_dir_type scatter_edges(icontext_type& context,
                               const vertex_type& vertex) const {
-    // If an iteration counter is set then
-    if (ITERATIONS) return graphlab::NO_EDGES;
     // In the dynamic case we run scatter on out edges if the we need
     // to maintain the delta cache or the tolerance is above bound.
-    if(USE_DELTA_CACHE || std::fabs(last_change) > TOLERANCE ) {
-      return graphlab::OUT_EDGES;
+    if( std::fabs(last_change) > TOLERANCE ) {
+      return parallelgraph::OUT_EDGES;
     } else {
-      return graphlab::NO_EDGES;
+      return parallelgraph::NO_EDGES;
     }
   }
 
   /* The scatter function just signal adjacent pages */
   void scatter(icontext_type& context, const vertex_type& vertex,
-               edge_type& edge) const {
-    if(USE_DELTA_CACHE) {
-      context.post_delta(edge.target(), last_change);
-    }
-
-    if(last_change > TOLERANCE || last_change < -TOLERANCE) {
-        context.signal(edge.target());
-    } else {
-      context.signal(edge.target()); //, std::fabs(last_change));
+               edge_type& edge) const 
+  {
+    if(last_change > TOLERANCE || last_change < -TOLERANCE) 
+    {
+        context.signal(edge.target().id());
+        std::cout<<"signal happend\n";
     }
   }
 
@@ -116,39 +87,20 @@ public:
 
 int main(int argc, char** argv) 
 {
-
-
-  graph_type graph(dc, clopts);
-  graph.finalize();
-  dc.cout() << "#vertices: " << graph.num_vertices()
+	//std::string file_path="/home/chen/ict/ParallelGraph/tests/dd";
+  std::string file_path= argv[1];
+	graph_type graph;
+	graph.directed=true;
+	graph.load(file_path,0.5);
+	graph.finalize();
+	
+  std::cout << "#vertices: " << graph.num_vertices()
             << " #edges:" << graph.num_edges() << std::endl;
 
-  // Initialize the vertex data
-  graph.transform_vertices(init_vertex);
-
-  // Running The Engine -------------------------------------------------------
-  graphlab::omni_engine<pagerank> engine(dc, graph, exec_type, clopts);
+  parallelgraph::Engine<pagerank> engine(graph);
   engine.signal_all();
-  engine.start();
-  const double runtime = engine.elapsed_seconds();
-  dc.cout() << "Finished Running engine in " << runtime
-            << " seconds." << std::endl;
-
-
-  const double total_rank = graph.map_reduce_vertices<double>(map_rank);
-  std::cout << "Total rank: " << total_rank << std::endl;
-
-  // Save the final graph -----------------------------------------------------
-  if (saveprefix != "") {
-    graph.save(saveprefix, pagerank_writer(),
-               false,    // do not gzip
-               true,     // save vertices
-               false);   // do not save edges
-  }
-
-  double totalpr = graph.map_reduce_vertices<double>(pagerank_sum);
-  std::cout << "Totalpr = " << totalpr << "\n";
-
+  engine.run();
+  graph.show_vertex();
   return 0;
 } // End of main
 
