d9ee69ca789f13ee6dc52e0d2e45a22bf5eb2fbe
chen <1728533217en@gmail.com>
2018-12-15 21:30:21 +0800

    add demo to show use of graphlab lib

---
+++ apps/demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <graphlab/rpc/dc.hpp>
+#include <graphlab/rpc/dc_init_from_mpi.hpp>
+//using namespace graphlab;
+
+int main(int argc, char ** argv) {
+  graphlab::mpi_tools::init(argc, argv);
+
+  graphlab::distributed_control dc;
+  std::cout<<" i am the rank :"<<dc.procid()<<std::endl;
+  dc.barrier();
+  graphlab::mpi_tools::finalize();
+}
